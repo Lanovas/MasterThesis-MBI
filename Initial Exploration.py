1176f88e73081077671479c14f22a85d2cad3a14
Lanovas <jamesmous13@gmail.com>
2019-02-18 22:14:47 +0100

    Initial commit

---
+++ Initial Exploration.py
@@ -0,0 +1,28 @@
+# Import the necessary packages
+import os
+import glob
+import pandas as pd
+import pymysql
+
+# Retrieve the working directory
+cwd = os.getcwd()
+
+# Load the data for analysis
+path = "SourceData/Patent Data/"
+extension = "csv"
+allFiles = glob.glob(pathname = path + "/*." + extension)
+
+patent_list = []
+#patent_set = [i for i in glob.glob('*.{}'.format(extension))]
+for file in allFiles:
+    df = pd.read_csv(file, delimiter = ";", header = 0, index_col = None)
+    patent_list.append(df)
+
+patent_set = pd.concat(objs = patent_list, axis = 0, ignore_index = True)
+
+# Data exploration
+print(patent_set.shape)
+
+
+
+#pymysql.connect
