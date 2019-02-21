1745a52c1ba236498c9c64c6848f2275a4a70101
Lanovas <jamesmous13@gmail.com>
2019-02-21 22:05:47 +0100

    Added subfolders

---
+++ Python Scripts/00. Database/DataTable Creation.py
@@ -0,0 +1,53 @@
+# Import the necessary packages
+import os
+import glob
+import pandas as pd
+import psycopg2 as pypos
+
+# Retrieve the working directory
+cwd = os.getcwd()
+
+# Load the data for analysis
+path = "SourceData/Patent Data/"
+extension = "csv"
+allFiles = glob.glob(pathname=path + "/*." + extension)
+
+patent_list = []
+# patent_set = [i for i in glob.glob('*.{}'.format(extension))]
+for file in allFiles:
+    df = pd.read_csv(file, delimiter=";", header=0, index_col=None)
+    patent_list.append(df)
+
+patent_set = pd.concat(objs=patent_list, axis=0, ignore_index=True)
+print(patent_set.shape)
+
+# Create a database with the full patent data set
+try:
+    connection = pypos.connect(user='postgres',
+                               password='Depp1323!',
+                               host='127.0.0.1',
+                               port=5432,
+                               database='MasterThesisData')
+    cursor = connection.cursor()
+    # Print PostgreSQL Connection properties
+    print(connection.get_dsn_parameters(), "\n")
+
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record, "\n")
+
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    # closing database connection.
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
+
+# Data exploration
+
+
+
+
