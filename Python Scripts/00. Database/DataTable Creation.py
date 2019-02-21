fabf8426ce4a563ea3c4f5fd5edc4b9d3cf2bf8d
Lanovas <jamesmous13@gmail.com>
2019-02-21 22:57:37 +0100

    Added the file connection to the login credentials

--- Python Scripts/00. Database/DataTable Creation.py
+++ Python Scripts/00. Database/DataTable Creation.py
@@ -1,13 +1,18 @@
 # Import the necessary packages
+import pandas as pd
 import psycopg2 as pypos
 
 # Create a database with the full patent data set
+pwd_gen = pd.read_csv(filepath_or_buffer="C:/Users/james/PycharmProjects/PWDGen.csv",
+                      sep=";", encoding="UTF-8")
+
 try:
-    connection = pypos.connect(user='postgres',
-                               password='Depp1323!',
-                               host='127.0.0.1',
-                               port=5432,
-                               database='MasterThesisData')
+    connection = pypos.connect(user=pwd_gen.loc[0, 'user'],
+                               password=pwd_gen.loc[0, 'password'],
+                               host=pwd_gen.loc[0, 'host'],
+                               port=pwd_gen.loc[0, 'port'],
+                               database=pwd_gen.loc[0, 'database'])
+
     cursor = connection.cursor()
     # Print PostgreSQL Connection properties
     print(connection.get_dsn_parameters(), "\n")
