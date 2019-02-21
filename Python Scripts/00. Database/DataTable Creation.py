38352c871c11ed830adfe5f3d7d540366f8ffd1d
Lanovas <jamesmous13@gmail.com>
2019-02-21 21:50:32 +0100

    Data table creation in postgreSQL - intermediate form

--- Python Scripts/00. Database/DataTable Creation.py
+++ Python Scripts/00. Database/DataTable Creation.py
@@ -1,25 +1,5 @@
 # Import the necessary packages
-import os
-import glob
-import pandas as pd
 import psycopg2
-
-# Retrieve the working directory
-cwd = os.getcwd()
-
-# Load the data for analysis
-path = "SourceData/Patent Data/"
-extension = "csv"
-allFiles = glob.glob(pathname = path + "/*." + extension)
-
-patent_list = []
-#patent_set = [i for i in glob.glob('*.{}'.format(extension))]
-for file in allFiles:
-    df = pd.read_csv(file, delimiter = ";", header = 0, index_col = None)
-    patent_list.append(df)
-
-patent_set = pd.concat(objs = patent_list, axis = 0, ignore_index = True)
-print(patent_set.shape)
 
 # Create a database with the full patent data set
 try:
