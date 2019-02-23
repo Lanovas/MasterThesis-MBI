48fbd555af3d7b3b0730c58a1f41088303eabc61
Lanovas <jamesmous13@gmail.com>
2019-02-23 21:56:01 +0100

    Changing the connection to MySQL and adding the respective coding block

--- Python Scripts/00. Database/DataTable Creation.py
+++ Python Scripts/00. Database/DataTable Creation.py
@@ -1,49 +1,40 @@
 # Import the necessary packages
 import pandas as pd
-import psycopg2 as pypos
+import mysql.connector
+from mysql.connector import errorcode
 
-# Create a database with the full patent data set
+# Connecting to the database
 pwd_gen = pd.read_csv(filepath_or_buffer="C:/Users/james/PycharmProjects/PWDGen.csv",
                       sep=";", encoding="UTF-8")
 
 try:
-    # connect to the PostgreSQL server
-    print("Connecting to the PostgreSQL database...")
+    # connect to the MySQL server
+    print("Connecting to the MySQL database...")
 
-    connection = pypos.connect(user=pwd_gen.loc[0, 'user'],
-                               password=pwd_gen.loc[0, 'password'],
-                               host=pwd_gen.loc[0, 'host'],
-                               port=pwd_gen.loc[0, 'port'],
-                               database=pwd_gen.loc[0, 'database'])
+    connection = mysql.connector.connect(user=pwd_gen.loc[0, 'user'],
+                                         password=pwd_gen.loc[0, 'password'],
+                                         host=pwd_gen.loc[0, 'host'],
+                                         port=pwd_gen.loc[0, 'port'],
+                                         database=pwd_gen.loc[0, 'database'])
 
-    cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    print(connection.get_dsn_parameters(), "\n")
+except mysql.connector.Error as error:
 
-    # Print PostgreSQL version
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record, "\n")
-
-    # Create the database tables needed for the patent data
-    commands = (
-        """
-        CREATE TABLE patent_data(
-        
-        
-        )
-        
-        """
-    )
-
-    # close the communication with the PostgreSQL
-    cursor.close()
-
-except (Exception, pypos.Error) as error:
-    print("Error while connecting to PostgreSQL", error)
+    if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif error.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(error)
 finally:
-    # closing database connection.
     if (connection):
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")
+        print("Connection was successful!")
+
+# Create a database with the full patent data set
+
+
+
+
+# closing database connection
+if (connection):
+    connection.close()
+    print("MySQL connection is closed")
