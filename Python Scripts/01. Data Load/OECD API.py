53934b1e9da0a2d096ff694ffaaf4fa8978aeb13
Lanovas <jamesmous13@gmail.com>
2019-03-12 20:45:02 +0100

    Adding the API connection file for OECD Part III

--- Python Scripts/01. Data Load/OECD API.py
+++ Python Scripts/01. Data Load/OECD API.py
@@ -1,15 +1,18 @@
 import pandasdmx as pdmx
 
-oecd_connection = pdmx.Request('OECD')
+oecd_connection = pdmx.api.Request('OECD')
 root_url = "https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/"
 dataset_identifier = "GOV"
 start_time = "2007"
 end_time = "2018"
 
 try:
-    data_response = oecd_connection.data()
-except:
-
-except:
-
+    data_response = oecd_connection.data(resource_id = dataset_identifier, key = 'all/all')
+except UnicodeDecodeError:
+    pass
+except KeyError:
+    pass
 finally:
+    oecd_dataset = data_response.data
+
+    df = data_response.write(oecd_dataset.series, parse_time=False)
